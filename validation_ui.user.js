2ebd28a2a1522dd99bd56e9f45e0ce5d9f228b93
Shannon Murdoch <contact@dreamscapemedia.com.au>
2015-02-26 21:53:26 +1100

    updated

---
+++ validation_ui.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Webjet validation overhaul
+// @namespace    http://webjet.com.au/
+// @version      0.1
+// @description  enter something useful
+// @author       Shannon Murdoch
+// @match        http://www.webjet.com.au/*
+// @match        https://www.webjet.com.au/*
+// @match        http://flights.webjet.com.au/*
+// @match        https://flights.webjet.com.au/*
+// @grant        none
+// ==/UserScript==
+
+$("head").append('<link href="http://www.shannonmurdoch.com/webjet/validation_ui/validation_ui.css" rel="stylesheet" type="text/css">');
+
+//$('body').css('background-color', 'red');
+console.log('done');
